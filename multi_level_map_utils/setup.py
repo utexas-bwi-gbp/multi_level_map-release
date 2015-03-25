a972f366f608853b263df8077ff64edb35354482
Piyush Khandelwal <piyushk@gmail.com>
2015-03-24 22:10:16 -0500

    Revert "Trimmed the branch to only the multi_level_map_server sub directory"
    This reverts commit 16eb00f3ad3efb27c62be23ae23c9be0bb91ca2f.

---
+++ multi_level_map_utils/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['multi_level_map_utils'],
+    package_dir={'': 'src'},
+    scripts={'nodes/level_mux', 'nodes/level_selector'},
+    )
+
+setup(**d)
